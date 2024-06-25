8e8a24913299bdeedfd2a3536419f8d39c472669
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-06-25 16:10:46 +0530

    About ngAfterViewInit

---
+++ tut-20-ngAfterViewInit/tut/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit,AfterViewInit{
+ 
+  title = 'tut';
+
+  @ViewChild('change') button!:ElementRef;
+
+  ngOnInit(): void {
+    console.log(this.button);
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.button.nativeElement);
+  }
+
+}
