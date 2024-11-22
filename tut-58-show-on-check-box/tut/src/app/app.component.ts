0ae5ac10839e53f091a5c599bc94ed436d0f32dc
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2024-11-22 09:17:56 +0530

    show data on checkbox button.

---
+++ tut-58-show-on-check-box/tut/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'tut';
+
+  b1 = false;
+  b2 = false;
+}
